0f2df8002a6e3f8157b9470aca84438f29ca8c39
Jacob Hendershott <jwhendershott@gmail.com>
2021-11-09 23:25:47 -0600

    created greeting function on day.js

--- day.js
+++ day.js
@@ -2,3 +2,7 @@ function dayName(date) {
     const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return daysOfTheWeek[date.getDay()];
 }
+
+function greeting(date) {
+    return `Hello, world! Happy ${dayName(date)}.`
+}
