4b72ce5f8b65216d36a11c8cbf401666ca69dca7
Jacob Hendershott <jwhendershott@gmail.com>
2021-12-14 00:55:52 -0600

    completed section 5

---
+++ foreach.js
@@ -0,0 +1,30 @@
+// let a = ["ant", "bat", "cat", 42];
+// a.forEach(function(element) {
+//     console.log(element);
+// });
+// let soliloquy = "To be, or not to be, that is the question.";
+
+// // for (let i = 0; i < soliloquy.length; i++) {
+// //     console.log(soliloquy.charAt(i));
+// // };
+
+// Array.from(soliloquy).forEach(function(element){
+//     console.log(element)
+// });
+
+// let a= [8, 17, 42, 99];
+// a.sort();
+
+// let a = ['ant', 'bat', 'cat', 42]
+
+// a.forEach((element) => {
+//     console.log(element);
+// });
+
+// let a = [1, -1, 0];
+// a.sort(function(a, b) { return a - b; });
+
+// let a = [8, 17, 42, 99];
+// a.forEach(function(element){
+//     console.log(element);
+// });
