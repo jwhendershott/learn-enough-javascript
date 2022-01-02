bc9ab9e51602e73eb75b5f584f4cec26d24e199a
Jacob Hendershott <jwhendershott@gmail.com>
2022-01-01 22:23:04 -0600

    created functional.js and imperativeUrls function

---
+++ functional.js
@@ -0,0 +1,15 @@
+let states = ["Kansas", "Nebraska", "North Dokota", "South Dakota"];
+
+// convert Kansas -> kansas
+// convert North Dakota -> north-dakota
+
+// urls: Imperative version
+function imperativeUrls(elements) {
+    let urls = [];
+    elements.forEach(function(element) {
+        urls.push(element.toLowerCase().split(/\s+/).join('-'))
+    });
+    return urls;
+}
+
+console.log(imperativeUrls(states));
