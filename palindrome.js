4eb4927b9a6a0ac4af1065f13a979633bad2f746
Jacob Hendershott <jwhendershott@gmail.com>
2022-01-19 23:08:36 -0600

    updated palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -8,22 +8,40 @@ function reverse(string) {
 function Phrase(content) {
     this.content = content;
 
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
+    }
+
+
     // Returns true for a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === reverse(this.processedContent());
     }
 
 }
 
-// Defines a Phrase object.
-function Phrase(content) {
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
     this.content = content;
+    this.translation = translation;
+
+        // Returns content processed for palindrome testing.
+        this.processedContent = function processedContent() {
+            return this.translation.toLowerCase();
+        }
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+// Defines a Phrase object.
+// function Phrase(content) {
+//     this.content = content;
   
-    // Makes the phrase LOUDER.
-    this.louder = function makeLouder() {
-      // FILL IN
-      let louderContent = this.content.toUpperCase();
-      return louderContent
-    };
-  }
+//     // Makes the phrase LOUDER.
+//     this.louder = function makeLouder() {
+//       // FILL IN
+//       let louderContent = this.content.toUpperCase();
+//       return louderContent
+//     };
+//   }
