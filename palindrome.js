0b046efb16b4f29bd372da92dda3de09ee59824f
Jacob Hendershott <jwhendershott@gmail.com>
2022-01-22 16:23:25 -0600

    updated palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
-// Reverses a string.
-function reverse(string) {
-    return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings
+String.prototype.reverse = function reverse() {
+    return Array.from(this).reverse().join("");
 }
 
 
@@ -16,23 +16,15 @@ function Phrase(content) {
 
     // Returns true for a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
     }
 
 }
 
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-        // Returns content processed for palindrome testing.
-        this.processedContent = function processedContent() {
-            return this.translation.toLowerCase();
-        }
+String.prototype.palindrome = function palindrome() {
+    return new Phrase(this).palindrome();
 }
 
-TranslatedPhrase.prototype = new Phrase();
 
 // Defines a Phrase object.
 // function Phrase(content) {
