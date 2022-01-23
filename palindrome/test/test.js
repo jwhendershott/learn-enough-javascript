6f35fa6aba284a37e3e2c75628377c5fb0ddc2fa
Jacob Hendershott <jwhendershott@gmail.com>
2022-01-22 19:49:38 -0600

    created test.js

---
+++ palindrome/test/test.js
@@ -0,0 +1,20 @@
+let assert = require('assert');
+let Phrase = require('../index.js');
+
+describe("Phrase", function(){
+
+    describe("#palindrome", function () {
+    
+        it("should return false for a non-palindrome", function (){
+            let nonPalindrome = new Phrase("apple");
+            assert(!nonPalindrome.palindrome());
+        });
+
+        it("should return false for a non-palindrome", function (){
+            let nonPalindrome = new Phrase("apple");
+            assert(!nonPalindrome.palindrome());
+        });
+
+        it("should return true for a mixed-case palindrome")
+    });
+});
