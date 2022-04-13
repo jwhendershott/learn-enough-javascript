71392e7a73e0fc8fb65a9b99339d7522d91b30d5
Jacob Hendershott <jwhendershott@gmail.com>
2022-04-12 21:08:19 -0500

    Finish palindrome detector

--- palindrome-page/main.js
+++ palindrome-page/main.js
@@ -1,13 +1,12 @@
 let Phrase = require("jwhendershott-palindrome");
 
-function palindromeTester() {
-        let string = prompt("Please enter a string for palindrome testing;");
-        let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+        let phrase = new Phrase(event.target.phrase.value);
         let palindromeResult = document.querySelector("#palindromeResult");
 
-    
         if (phrase.palindrome()) {
-        palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
+        palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
     } else {
         palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
     }
@@ -15,7 +14,7 @@ function palindromeTester() {
 
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.querySelector("#palindromeTester");
-    button.addEventListener("click", function() {
-        palindromeTester();
+    button.addEventListener("submit", function() {
+        palindromeTester(event);
     });
 });
